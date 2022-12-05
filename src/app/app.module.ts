e8c5e5842d53fa48922c42488355442629ae3787
Igor Lopes <loadioliveira@gmail.com>
2022-12-05 10:49:23 -0300

    criando header-component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,11 +5,13 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { HomeComponent } from './pages/home/home.component';
+import { HeaderComponentComponent } from './components/header-component/header-component.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomeComponent
+    HomeComponent,
+    HeaderComponentComponent
   ],
   imports: [
     BrowserModule,
